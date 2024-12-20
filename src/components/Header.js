f3c519c232557eeeff7c4dd0bc307b730f9ae0c0
Shikhar-blockchain@gmail.com <shikharshukla5525678@gmail.com>
2024-12-20 16:56:57 +0530

    Resolve merge conflicts

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,7 @@
 import { useState } from "react";
 import { LOGO_URL } from "../utils/Constants";
-<<<<<<< HEAD
-=======
+
 import { Link } from "react-router-dom";
->>>>>>> 720fe30 (Initial commit)
 
 const Header = () => {
   const [btnName, setBtnName] = useState("login");
@@ -17,12 +15,11 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul className="flex ecimal text-2xl p-4 m-4">
-<<<<<<< HEAD
           <li className="p-4">Home</li>
           <li className="p-4">About Us</li>
           <li className="p-4">Contact Us</li>
           <li className="p-4">Cart</li>
-=======
+
           <li className="p-4">
             <Link to={"/"}>Home</Link>
           </li>
@@ -35,7 +32,7 @@ const Header = () => {
           <li className="p-4">
             <Link>Cart</Link>
           </li>
->>>>>>> 720fe30 (Initial commit)
+
           <button
             className="bg-black text-white text-sm px-4 h-6 inline-block align-middle mt-[23] rounded-xl border border-blue-700"
             onClick={() => {
